ff656ec2ce6529d324e5d7f9b1ab5c442cfcefc4
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-07-11 19:08:25 +0200

    Finalizando reducers y eliminar favoritos

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { setFavorite } from '../actions';
+import { setFavorite, deleteFavorite } from '../actions';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
+import removeIcon from '../assets/static/remove-icon.png';
 
 const CarouselItem = (props) => {
   const { id, cover, title, year, contentRating, duration } = props;
@@ -12,6 +13,9 @@ const CarouselItem = (props) => {
     props.setFavorite({
       id, cover, title, year, contentRating, duration,
     });
+  };
+  const handleDeleteFavorite = (itemId) => {
+    props.deleteFavorite(itemId);
   };
   return (
     <div className='carousel-item'>
@@ -24,6 +28,12 @@ const CarouselItem = (props) => {
             src={plusIcon}
             alt='Plus Icon'
             onClick={handleSetFavorite}
+          />
+          <img
+            className='carousel-item__details--img'
+            src={removeIcon}
+            alt='Plus Icon'
+            onClick={() => handleDeleteFavorite(id)}
           />
         </div>
         <p className='carousel-item__details--title'>{title}</p>
@@ -43,6 +53,7 @@ CarouselItem.propTypes = {
 
 const mapDispatchToProps = {
   setFavorite,
+  deleteFavorite,
 };
 
 export default connect(null, mapDispatchToProps)(CarouselItem);
