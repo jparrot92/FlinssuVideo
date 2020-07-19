686a190da49c7fd5df738922fb077d2fbfc33ada
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-07-19 20:21:42 +0200

    Agregar funcionalidad del player

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
 import '../assets/styles/components/Player.scss';
+import NotFound from './NotFound';
 
-const Player = () => {
-  return (
+const Player = (props) => {
+  const { id } = props.match.params;
+  const [loading, setLoading] = useState(true);
+  const hasPlaying = Object.keys(props.playing).length > 0;
+
+  useEffect(() => {
+    props.getVideoSource(id);
+    setLoading(false);
+  }, []);
+
+  if (loading) return <h2>Cargando video...</h2>;
+
+  return hasPlaying ? (
     <div className='Player'>
       <video controls autoPlay>
-        <source src='' type='video/mp4' />
+        <source src={props.playing.source} type='video/mp4' />
       </video>
       <div className='Player-back'>
         <button type='button' onClick={() => props.history.goBack()}>
@@ -13,7 +27,17 @@ const Player = () => {
         </button>
       </div>
     </div>
-  );
+  ) : <NotFound />;
 };
 
-export default Player;
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+
+const mapDispatchToProps = {
+  getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
